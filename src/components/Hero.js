a2907f7d8e0b9f893dfa06273f19c16866bb0331
Ajeet jha <jhaajeetcs@gmail.com>
2020-10-14 20:16:27 +0530

    build and deploy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Spinner from 'react-bootstrap/Spinner'
 
 function Hero(props) {
 
@@ -13,7 +14,7 @@ function Hero(props) {
                     <Col md={8} sm={12}>
                         { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
                         { props.subTitle && <h3 className="display-4 font-weight-light">{props.subTitle}</h3> }
-                        { props.text && <h3 className="lead font-weight-light">{props.text}</h3> }
+                        { props.text && <h3 className="lead font-weight-light">{props.text} { props.text === "Site under maintinance" ? <><Spinner animation="grow" size="sm" /><Spinner animation="grow" size="sm" /><Spinner animation="grow" size="sm" /></>:""}</h3> }
                     </Col>
                 </Row>
             </Container>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,28 +11,28 @@ class HomePage extends Component {
                 border: "primary",
                 variant: "top",
                 src : "https://lh3.googleusercontent.com/-V1bYen-_uBc/X4cBxiYuKpI/AAAAAAABA10/dFOCTJmpK-kH1cfHgOi7eNA7Si-rk4kigCK8BGAsYHg/s0/2020-10-14.jpg",
-                card_title: "Primary Card Title",
+                card_title: "Working selfie",
                 card_header: "Header",
-                card_mute: "Last updated 3 mins ago",
-                card_text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                card_mute: "Last updated 1 years ago",
+                card_text: "I'm a bit of a workaholic. When I feel like I'm not doing something, it drives me insane."
             },
             {
                 border: "danger",
                 variant: "top",
                 src : "https://lh3.googleusercontent.com/-oWE0h3mJAX4/X4cBwyEjChI/AAAAAAABA1o/Pe398YFN37cQjmRDiTwO6uA7Ybn1DgtFACK8BGAsYHg/s0/2020-10-14.jpg",
-                card_title: "Primary Card Title",
+                card_title: "Once in the blue moon",
                 card_header: "Header",
-                card_mute: "Last updated 3 mins ago",
-                card_text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                card_mute: "Last updated 6 months ago",
+                card_text: "Ham toh bhai jaise hain, waise rahenge Ab koyi khush ho, ya ho khafa, ham nahin badlenge, apni adaa Samjhe na samjhe koyi ham yahi kahenge."
             },
             {
                 border: "info",
                 variant: "top",
                 src : "https://lh3.googleusercontent.com/-ygd7vAKtnts/X4cBxZ_Lk2I/AAAAAAABA1w/Cn2XykhoqkQwStahI_cGVlamzN9L8icnACK8BGAsYHg/s0/2020-10-14.jpg",
-                card_title: "Primary Card Title",
+                card_title: "Chatkara",
                 card_header: "Header",
-                card_mute: "Last updated 3 mins ago",
-                card_text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+                card_mute: "Last updated 4 year ago",
+                card_text: "Do me padha toh kafi nai tha kya ????????? 3rd me kya padh k mil jayega"
             }]
         };
     }
